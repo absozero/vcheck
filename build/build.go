c673d54653ffb597077dc244b457f7f39d15cbce
Absozero <aswsub123@gmail.com>
2023-10-13 03:36:29 -0700

    Added boilerplate

---
+++ build/build.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/absozero/vckeck/cmd/vcheck"
+  )
+  
+  func main() {
+	vckeck.Execute()
+  }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/absozero/vcheck
 
 go 1.21.1
+
+require (
+        "github.com/spf13/cobra" v1.7.0
+)
