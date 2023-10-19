057df214887e57038f86859d6a7eceae22bf0dba
Absozero <azermazer165@gmail.com>
2023-10-18 18:31:44 -0700

    Decided on subfolder based subcommand directory structure.

--- build/build.go
+++ build/build.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/absozero/vcheck/cmd/vcheck"
+  _ "github.com/absozero/vcheck/cmd/vcheck/version"
+
   )
   
   func main() {
-	vcheck.Execute()
+	cmd.Execute()
   }
