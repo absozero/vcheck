f6c501cc4163fcec7d46d5b4e20cdec248c9cb32
Absozero <aswsub123@gmail.com>
2023-10-20 18:09:48 -0700

    Fix: Missing directories put into gitignore brought back

---
+++ cmd/vcheck/get/get.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package version
+
+import (
+	"github.com/absozero/vcheck/cmd/vcheck"
+	"github.com/absozero/vcheck/pkg/vercheck"
+	"github.com/spf13/cobra"
+	
+)
+
+// helpCmd represents the help command
+var VersionCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Returns the latest version of a given project on github",
+	Long: `Shows the current version of vcheck in the program.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		vercheck.GithubCall()
+	},
+}
+
+func init() {
+
+	cmd.VcheckCmd.AddCommand(VersionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// helpCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// helpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/vcheck/root.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+
+
+)
+
+var VcheckCmd = &cobra.Command{
+	Use:   "vcheck",
+	Short: "A version checker.",
+	Long:  `A utility made to check versions of software and track software versions effectively.`,
+
+}
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := VcheckCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.solus.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	VcheckCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
